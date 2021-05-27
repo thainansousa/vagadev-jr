398f9b55c8d8d30c09417bc3c4bb658094c72543
Thainan <thainansousa7@gmail.com>
2021-05-26 21:03:59 -0300

    mobile first new version correction bug frame 320px

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,51 +5,48 @@ import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-
-      <section>
-        <div className={styles.banner_container}>
-          <img src="/img/principal_banner_mobile.jpg" alt="banner 01" />
-
-          <div className={styles.info_banner_container}>
-            <div className={styles.info_banner}>
-              <h2>Mortal Kombat</h2>
-              <div>
-                <h1>R$ 299</h1>
-                <span>,99</span>
-              </div>
-              <p>
-                Mortal Kombat X combina uma apresentação cinemática única com uma jogabilidade totalmente nova.
-                Os jogadores podem escolher pela primeira vez diversas variantes de cada personagem, afetando
-                tanto a estratégia como o estilo de luta.
-            </p>
-            </div>
-          </div>
-        </div>
-        <div className={styles.navegation_container}>
-          <div className={styles.game}>
-            <span>Mortal Kombat</span>
-          </div>
-          <div className={styles.navegation}>
+    <div className={styles.container}>
+      <div className={styles.banner_container}>
+        <Header />
+        <img src="/img/principal_banner_mobile.jpg" alt="banner 01" className={styles.banner} />
+        <div className={styles.info_banner_container}>
+          <div className={styles.info_banner}>
+            <h2> Mortal Kombat</h2>
             <div>
-              <span>1</span>
-              <span>/</span>
-              <span>2</span>
+              <h1>R$ 299</h1>
+              <span>,99</span>
             </div>
-            <img src="/svgs/angle-left-solid.svg" alt="back" />
-            <img src="/svgs/angle-right-solid.svg" alt="next" />
+            <p>
+              Mortal Kombat X combina uma apresentação cinemática única com uma jogabilidade totalmente nova.
+              Os jogadores podem escolher pela primeira vez diversas variantes de cada personagem, afetando
+              tanto a estratégia como o estilo de luta.
+            </p>
           </div>
         </div>
-      </section>
-
+      </div>
+      <div className={styles.navegation_container}>
+        <div className={styles.game}>
+          <span>Mortal Kombat</span>
+        </div>
+        <div className={styles.navegation}>
+          <div>
+            <span>1</span>
+            <span>/</span>
+            <span>2</span>
+          </div>
+          <img src="/svgs/angle-left-solid.svg" alt="back" />
+          <img src="/svgs/angle-right-solid.svg" alt="next" />
+        </div>
+      </div>
       <section className={styles.spotlight_container}>
-        <div className={styles.middle_banner_01}>
+        <div className={styles.middle_banner}>
+          <img src="/img/zelda_banner.jpg" alt="" />
           <div className={styles.middle_banner_info}>
             <p>The Legend of Zelda - Breath of th wild</p>
           </div>
         </div>
-        <div className={styles.middle_banner_02}>
+        <div className={styles.middle_banner}>
+          <img src="/img/sekiro_banner.jpg" alt="" />
           <div className={styles.middle_banner_info}>
             <p>SEKIRO - Shadows die twice</p>
           </div>
@@ -73,8 +70,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-
       <Footer />
-    </div>
+    </div >
   )
 }
